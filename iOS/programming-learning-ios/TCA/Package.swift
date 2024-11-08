3ec3bf74f05b04e4ce8e1800340ec38345e55b31
Kohei Araya <kohi218ponmiso@gmail.com>
2024-11-08 13:24:57 +0900

    TCAライブラリを追加

--- iOS/programming-learning-ios/TCA/Package.swift
+++ iOS/programming-learning-ios/TCA/Package.swift
@@ -5,17 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "TCA",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TCA",
             targets: ["TCA"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            from: "1.0.0"
+        ),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TCA"),
+            name: "TCA",
+            dependencies: [
+                .product(
+                    name: "ComposableArchitecture",
+                    package: "swift-composable-architecture"
+                )
+            ]
+        ),
         .testTarget(
             name: "TCATests",
             dependencies: ["TCA"]
