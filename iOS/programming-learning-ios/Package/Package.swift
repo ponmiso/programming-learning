bcb5a79495d065eceac86a56129d4bc61c0c875a
Kohei Araya <kohi218ponmiso@gmail.com>
2024-12-02 13:31:20 +0900

    Package側でOpenAPIを扱えるようにした

--- iOS/programming-learning-ios/Package/Package.swift
+++ iOS/programming-learning-ios/Package/Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "Package",
+    platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "API",
             targets: ["API"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.2"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "API"),
+            name: "API",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+            ],
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+            ]),
         .testTarget(
             name: "APITests",
             dependencies: ["API"]
