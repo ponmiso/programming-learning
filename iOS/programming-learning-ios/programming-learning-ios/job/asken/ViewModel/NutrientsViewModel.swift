b31725ab0ec82cbc9e424f0ecdd35e102bc978d2
Kohei Araya <kohi218ponmiso@gmail.com>
2025-06-02 19:42:08 +0900

    白米と納豆を分けて算出できるようにした

--- iOS/programming-learning-ios/programming-learning-ios/job/asken/ViewModel/NutrientsViewModel.swift
+++ iOS/programming-learning-ios/programming-learning-ios/job/asken/ViewModel/NutrientsViewModel.swift
@@ -5,14 +5,20 @@ struct NutrientsViewModel {
     }
 
     var nattoRiceCalorie: Int {
+        riceCalorie + nattoCalorie
+    }
+    
+    var riceCalorie: Int {
         let rice = Ingredients(protein: Protein(gram: 2.5),
                                fat: Fat(gram: 0.3),
                                carbohydrate: Carbohydrate(gram: 37.1))
+        return repository.toCalorie(ingredients: rice)
+    }
+    
+    var nattoCalorie: Int {
         let natto = Ingredients(protein: Protein(gram: 16.5),
                                 fat: Fat(gram: 10.0),
                                 carbohydrate: Carbohydrate(gram: 12.1))
-        let riceCalorie = repository.toCalorie(ingredients: rice)
-        let nattoCalorie = repository.toCalorie(ingredients: natto)
-        return riceCalorie + nattoCalorie
+        return repository.toCalorie(ingredients: natto)
     }
 }
--- iOS/programming-learning-ios/programming-learning-iosTests/NutrientsTests.swift
+++ iOS/programming-learning-ios/programming-learning-iosTests/NutrientsTests.swift
@@ -6,11 +6,12 @@ struct NutrientsTests {
     @Test func 納豆ご飯のカロリーが正しいこと() {
         let repository = NutrientsRepository()
         let viewModel = NutrientsViewModel(repository: repository)
-        let actual = viewModel.nattoRiceCalorie
         
         let riceCalorie = Int(((2.5 * 4.0) + (0.3 * 9.0) + (37.1 * 4.0)).rounded())
         let nattoCalorie = Int(((16.5 * 4.0) + (10.0 * 9.0) + (12.1 * 4.0)).rounded())
-        let expected = riceCalorie + nattoCalorie
-        #expect(actual == expected)
+        
+        #expect(viewModel.nattoRiceCalorie == riceCalorie + nattoCalorie)
+        #expect(viewModel.riceCalorie == riceCalorie)
+        #expect(viewModel.nattoCalorie == nattoCalorie)
     }
 }
