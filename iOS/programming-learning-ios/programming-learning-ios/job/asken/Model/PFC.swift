d238de231977627f077ee57ee7d149ab74b7b8a6
Kohei Araya <kohi218ponmiso@gmail.com>
2025-06-02 18:09:14 +0900

    グラムからカロリーに変換するためのプロトコルを作成

--- iOS/programming-learning-ios/programming-learning-ios/job/asken/Model/PFC.swift
+++ iOS/programming-learning-ios/programming-learning-ios/job/asken/Model/PFC.swift
@@ -1,5 +1,27 @@
-struct Protein {}
+protocol PFC {
+    /// グラム(g)
+    var gram: Double { get }
+    /// 1グラム(g)あたりのカロリー(kcal)
+    var caloriePerGram: Double { get }
+}
 
-struct Fat {}
+struct Protein: PFC {
+    let gram: Double
+    var caloriePerGram: Double {
+        4.0
+    }
+}
 
-struct Carbohydrate {}
+struct Fat: PFC {
+    let gram: Double
+    var caloriePerGram: Double {
+        9.0
+    }
+}
+
+struct Carbohydrate: PFC {
+    let gram: Double
+    var caloriePerGram: Double {
+        4.0
+    }
+}
