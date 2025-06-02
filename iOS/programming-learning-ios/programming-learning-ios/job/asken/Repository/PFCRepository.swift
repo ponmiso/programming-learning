c513689b3754ee56a653d474e593b810df91d2c6
Kohei Araya <kohi218ponmiso@gmail.com>
2025-06-02 18:41:39 +0900

    納豆ご飯のカロリーを求めるプロパティを追加

--- iOS/programming-learning-ios/programming-learning-ios/job/asken/Repository/PFCRepository.swift
+++ iOS/programming-learning-ios/programming-learning-ios/job/asken/Repository/PFCRepository.swift
@@ -1,10 +1,18 @@
 import Foundation
 
 protocol PFCRepositoryProtocol {
+    func toCalorie(ingredients: Ingredients) -> Int
     func toCalorie(nutrients: Nutrients) -> Int
 }
 
 struct PFCRepository: PFCRepositoryProtocol {
+    func toCalorie(ingredients: Ingredients) -> Int {
+        let proteinCalorie = toCalorie(nutrients: ingredients.protein)
+        let fatCalorie = toCalorie(nutrients: ingredients.fat)
+        let carbohydrateCalorie = toCalorie(nutrients: ingredients.carbohydrate)
+        return proteinCalorie + fatCalorie + carbohydrateCalorie
+    }
+    
     func toCalorie(nutrients: Nutrients) -> Int {
         let roundedCalorie = (nutrients.gram * nutrients.caloriePerGram).rounded()
         return Int(roundedCalorie)
---
+++ iOS/programming-learning-ios/programming-learning-ios/job/asken/ViewModel/PFCViewModel.swift
@@ -0,0 +1,18 @@
+struct PFCViewModel {
+    let repository: PFCRepositoryProtocol
+    init(repository: PFCRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    var nattoRiceCalorie: Int {
+        let rice = Ingredients(protein: Protein(gram: 2.5),
+                               fat: Fat(gram: 0.3),
+                               carbohydrate: Carbohydrate(gram: 37.1))
+        let natto = Ingredients(protein: Protein(gram: 16.5),
+                                fat: Fat(gram: 10.0),
+                                carbohydrate: Carbohydrate(gram: 12.1))
+        let riceCalorie = repository.toCalorie(ingredients: rice)
+        let nattoCalorie = repository.toCalorie(ingredients: natto)
+        return riceCalorie + nattoCalorie
+    }
+}
