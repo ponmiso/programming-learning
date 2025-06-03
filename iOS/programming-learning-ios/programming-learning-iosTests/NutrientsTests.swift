fc89a0bbb651e798ac3908b4ad3444e6ced5b113
Kohei Araya <kohi218ponmiso@gmail.com>
2025-06-03 16:13:56 +0900

    テスト観点のコメントを追記

--- iOS/programming-learning-ios/programming-learning-iosTests/NutrientsTests.swift
+++ iOS/programming-learning-ios/programming-learning-iosTests/NutrientsTests.swift
@@ -5,10 +5,10 @@ struct NutrientsTests {
     /// 問題2のテスト
     @Test func 納豆ご飯のカロリーが正しいこと() {
         let viewModel = NutrientsViewModel()
-        
         let riceCalorie = Int(((2.5 * 4.0) + (0.3 * 9.0) + (37.1 * 4.0)).rounded())
         let nattoCalorie = Int(((16.5 * 4.0) + (10.0 * 9.0) + (12.1 * 4.0)).rounded())
         
+        // 納豆ご飯の総カロリーと、納豆・白米それぞれの総カロリーが正しいかを確認
         #expect(viewModel.nattoRiceCalorie == riceCalorie + nattoCalorie)
         #expect(viewModel.riceCalorie == riceCalorie)
         #expect(viewModel.nattoCalorie == nattoCalorie)
